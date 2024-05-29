f4353f34e0cc1b02578531a6e8c101a8aceb5573
HyunSuk <rlafovj@gmail.com>
2024-05-29 17:03:10 +0900

    refactor: Change testData.sql year value

--- src/main/resources/testData.sql
+++ src/main/resources/testData.sql
@@ -1,5 +1,5 @@
 insert into users (email, password, nickname, gender, birth_date) values ('qwerty', '1234', '지나가는여행자', 1, '2024-05-29');
-insert into users (email, password, nickname, gender, birth_date) values ('qwerty2', '1234', '누워있는개백수', 0, '1111-01-11');
-insert into users (email, password, nickname, gender, birth_date) values ('qwerty3', '1234', '코딩하는개발자', 1, '2222-02-22');
-insert into users (email, password, nickname, gender, birth_date) values ('qwerty4', '1234', '야근하는직장인', 0, '3333-03-01');
-insert into users (email, password, nickname, gender, birth_date) values ('qwerty5', '1234', '노래하는아이돌', 1, '4444-04-01');
+insert into users (email, password, nickname, gender, birth_date) values ('qwerty2', '1234', '누워있는개백수', 0, '1990-01-11');
+insert into users (email, password, nickname, gender, birth_date) values ('qwerty3', '1234', '코딩하는개발자', 1, '2022-02-22');
+insert into users (email, password, nickname, gender, birth_date) values ('qwerty4', '1234', '야근하는직장인', 0, '2033-03-01');
+insert into users (email, password, nickname, gender, birth_date) values ('qwerty5', '1234', '노래하는아이돌', 1, '2044-04-01');
