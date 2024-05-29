d074bfe4f4212727623182ac97b17e11c115b6a6
HyunSuk <rlafovj@gmail.com>
2024-05-29 18:00:14 +0900

    refactor: Delete auth package

--- src/main/java/com/tetrips/api/auth/AuthService.java
+++
@@ -1,8 +0,0 @@
-package com.tetrips.api.auth;
-
-import com.tetrips.api.common.MessengerVO;
-import com.tetrips.api.user.UserDTO;
-
-public interface AuthService {
-  public MessengerVO login(UserDTO param);
-}
