f0fcb79e02ba449e813bc6b28c76d36daf534929
rlafovj <rlafovj@gmail.com>
2024-05-31 00:53:26 +0900

    feat: Add AuthInterceptor validateToken logic

--- src/main/java/com/tetrips/api/common/interceptor/AuthInterceptor.java
+++ src/main/java/com/tetrips/api/common/interceptor/AuthInterceptor.java
@@ -24,12 +24,13 @@ public class AuthInterceptor implements HandlerInterceptor {
             .map(i -> jwtProvider.extractTokenFromHeader(i))
             .filter(i -> !i.equals("undefined token"))
             .peek(token -> log.info("1- 인터셉터 토큰 로그 Bearer 포함 : {}", token))
-            //vaildate 토큰 검증 시퀀스 추가 필요
+            .filter(i -> jwtProvider.validateToken(i, "access"))
             .map(i -> jwtProvider.getPayload(i).get("userEmail", String.class))
             .map(email -> userRepository.findUserByEmail(email))
-            //.filter(i -> i.isPresent())
+            .filter(i -> i.isPresent())
             .peek(email -> log.info("2- 인터셉트사용자 Email : {}", email))
             .findFirst()
             .isPresent();
+
 }
 }
